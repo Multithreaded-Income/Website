b92e3bf4f0c8ce7fd2c63f789db2c71c4e454d61
Kevin Griffin <kevin@kevgriffin.com>
2023-11-21 16:15:41 -0500

    Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    redirects: {
-        "discord" : "https://discord.gg/sJhrJcVAzk",
-        "podcast" : "https://podcast.multithreadedincome.com"
-    }
+  redirects: {
+    "discord": "https://discord.gg/sJhrJcVAzk",
+    "podcast": "https://podcast.multithreadedincome.com"
+  },
+  integrations: [tailwind()]
 });
