eaa86f497e0594f0bb2523b01348a85b59053872
Kevin Griffin <kevin@kevgriffin.com>
2023-11-20 22:32:39 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    redirects: {
+        "discord" : "https://discord.gg/sJhrJcVAzk",
+        "podcast" : "https://podcast.multithreadedincome.com"
+    }
+});
---
+++ src/content/articles/2023-11-20 Sample Article.md
@@ -0,0 +1,16 @@
+---
+title: Sample Article
+slug: sample-article
+date: 2023-11-20
+author: Kevin Griffin
+categories:
+    - Technology
+    - Programming
+tags:
+    - Markdown
+    - Front Matter
+---
+
+# Sample Article
+
+This is a sample article written in Markdown
