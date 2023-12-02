aa3977ab0fd252e14fdaae0fa1250ddc3d2f4538
Kevin Griffin <kevin@kevgriffin.com>
2023-12-02 11:34:31 -0500

    build tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://multithreadedincome.com",
   redirects: {
     "discord": "https://discord.gg/sJhrJcVAzk",
     "podcast": "https://podcast.multithreadedincome.com"
