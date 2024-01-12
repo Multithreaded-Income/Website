7c2ebd1bca0bd9700e59e034ec6104869922cfe2
Kevin Griffin <kevin@kevgriffin.com>
2024-01-12 17:18:40 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://multithreadedincome.com",
   redirects: {
-    "discord": "https://discord.gg/sJhrJcVAzk",
+    // "discord": "https://discord.gg/sJhrJcVAzk",
     "podcast": "https://podcast.multithreadedincome.com"
   },
   integrations: [tailwind(), sitemap()]
